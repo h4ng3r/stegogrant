14103ee6074536341e8c2e3d2eea1679ee51fcb8
h4ng3r <h4ng3r@users.noreply.github.com>
2017-02-05 23:05:34 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# Main packages
+sudo apt-get update
+
+# Install python and pip
+sudo apt-get -y install python-pip
+
+# Install steganography
+sudo apt-get -y install zlib1g-dev
+sudo apt-get -y install libjpeg62-dev
+sudo apt-get -y install python-dev
+sudo pip install steganography
+
+# Install steghide
+sudo apt-get -y install steghide
+
+#sudo apt-get -y install openjdk-7-jdk
